b457009b913934d837bd98b7348ccdbbc1a6226a
n00bhexc0der <vulbugfinder@gmail.com>
2014-02-10 16:26:44 +0530

    Notes
    Wait for updates

---
+++ nav/nav2.js
@@ -0,0 +1,84 @@
+function create_menu(basepath)
+{
+	var base = (basepath == 'null') ? '' : basepath;
+	base = base + ''
+	document.write(
+		'<table cellpadding="0" cellspaceing="0" border="0" style="width:98%"><tr>' +
+		'<h3>SecurityTube Metasploit Framework Expert Video List </h3>' +
+        '<td class="td_sep" valign="top">' +
+		'<ul>' +
+			'<li><a href="../start.html">Course Home </a></li>' +
+			'<li><a href="'+base+'001.html">Part 1: Exploitation Basics</a></li>' +
+			'<li><a href="'+base+'002.html">Part 2: Why Metasploit</a></li>' +
+			'<li><a href="'+base+'003.html">Part 3: Meterpreter Basics</a></li>' +
+			'<li><a href="'+base+'004.html">Part 4: Framework Organization</a></li>' +
+			'<li><a href="'+base+'005.html">Part 5: Post Exploitation Kung-Fu</a></li>' +
+			'<li><a href="'+base+'006.html">Part 6: Post Exploitation Privilege Escalation</a></li>' +
+			'<li><a href="'+base+'007.html">Part 7: Killing AV and Disabling Firewall</a></li>' +
+			'<li><a href="'+base+'008.html">Part 8: Stdapi and Priv Extensions</a></li>' +
+			'<li><a href="'+base+'009.html">Part 9: Token Stealing and Incognito</a></li>' +
+			'<li><a href="'+base+'010.html">Part 10: Espia and Sniffer Extensions</a></li>' +
+			'<li><a href="'+base+'011.html">Part 11: Post Exploitation Backdoors</a></li>' +
+			'<li><a href="'+base+'012.html">Part 12: Pivoting after Post Exploitation</a></li>' +
+			'<li><a href="'+base+'013.html">Part 13 : Port Forwarding as part of Post Exploitation</a></li>' +
+			'<li><a href="'+base+'014.html">Part 14: Client Side Exploits</a></li>' +
+			'<li><a href="'+base+'015.html">Part 15: Backdoors and Rootkits in Post Exploitation</a></li>' +
+			'<li><a href="'+base+'016.html">Part 16: Exploit Research with Metasploit</a></li>' +
+			'<li><a href="'+base+'017.html">Part 17: Railgun Basics</a></li>' +
+			'<li><a href="'+base+'018.html">Part 18: Railgun Adding Functions</a></li>' +
+			'<li><a href="'+base+'019.html">Part 19: Railgun Adding New DLLs</a></li>' +
+			'<li><a href="'+base+'020.html">Part 20: Resource Scripts</a></li>' +
+			
+
+
+			'</ul>' +
+			'</td><td class="td_sep" valign="top">' +
+			'<h3></h3>'+
+			'<ul>' +
+			
+			'<li><a href="'+base+'021.html">Part 21: Database Support</a></li>' +
+			'<li><a href="'+base+'022.html">Part 22: Using Plugins</a></li>' +
+			'<li><a href="'+base+'023.html">Part 23: Meterpreter API Basics</a></li>' +
+			'<li><a href="'+base+'024.html">Part 24: Meterpreter Scripting - Migrate Clone</a></li>' +
+			'<li><a href="'+base+'025.html">Part 25: Meterpreter Scripting - Process Name Search</a></li>' +
+			'<li><a href="'+base+'026.html">Part 26: Social Engineering Toolkit Java Applet</a></li>' +
+			'<li><a href="'+base+'027.html">Armitage Demo by Raphael Mudge</a></li>' +
+			'<li><a href="'+base+'028.html">Exploit Research Part 1: Introduction</a></li>' +
+			'<li><a href="'+base+'029.html">Exploit Research Part 2: Memcpy Buffer Overflow</a></li>' +
+			'<li><a href="'+base+'030.html">Exploit Research Part 3: Strcpy Buffer Overflow</a></li>' +
+			'<li><a href="'+base+'031.html">Exploit Research Part 4: Minishare Buffer Overflow</a></li>' +
+			'<li><a href="'+base+'032.html">Exploit Research Part 5: FreeSSHD Buffer Overflow</a></li>' +
+			'<li><a href="'+base+'033.html">Exploit Research Part 6: SEH Basics</a></li>' +
+			'<li><a href="'+base+'034.html">Exploit Research Part 7: Overwrite SEH</a></li>' +
+			'<li><a href="'+base+'035.html">Exploit Research Part 8: Exploiting SEH</a></li>' +
+			
+
+			'</ul>' +
+			'</td><td class="td_sep" valign="top">' +
+			'<h3>Other Certifications and Videos from SecurityTube</h3>'+
+			'<ul>' +
+			'<li><a href="http://securitytube-training.com/certifications/securitytube-wi-fi-security-expert/">SecurityTube Wi-Fi Security Expert Certification</a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=6">Windows Assembly Language Megaprimer </a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=5">Linux Assembly Language Megaprimer </a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=7">Exploit Research Megaprimer </a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=2">Scenario Based Hacking </a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=4">Linux Buffer Overflow Megaprimer</a></li>' +
+			'<li><a href="http://www.securitytube.net/groups?operation=view&groupId=1">Router Hacking Megaprimer</a></li>' +
+		
+			'</ul>' +
+			'</td><td class="td_sep" valign="top">' +
+			'<h3>About Us</h3>'+
+			'<ul>' +
+			'<li><a href="http://www.securitytube.net">SecurityTube Home </a></li>' +
+			
+			'<li><a href="http://securitytube-training.com/">SecurityTube Certifications</a></li>' +
+
+
+
+
+
+
+		        '</ul>' +
+				
+		'</td></tr></table>');
+}
